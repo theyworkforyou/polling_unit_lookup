3a3a59d6763ea04fd914ce08cd70e7bf46e2cab9
Chris Mytton <chrismytton@gmail.com>
2016-02-23 16:32:12 +0000

    Lookup PU number using the MapIt API

--- polling_unit_lookup.py
+++ polling_unit_lookup.py
@@ -1,17 +1,38 @@
-from flask import Flask, request
+import re
+
+import requests
+from flask import Flask, request, redirect
+
 from polling_unit import tidy_up_pun, pun_re
 
 
 app = Flask(__name__)
 
+mapit_url_template = 'http://www.shineyoureye.org/mapit/code/poll_unit/{pun}'
+
 
 @app.route("/")
 def polling_unit_lookup():
-    pun = tidy_up_pun(request.args.get('q'))
-    if pun_re.search(pun):
-        return "{} is a valid polling unit number".format(pun)
-    else:
-        return "{} is not a valid polling unit number".format(pun)
+    query = request.args.get('q')
+    pun = tidy_up_pun(query)
+
+    if not pun_re.search(pun):
+        return "{} is not a valid polling unit number".format(query)
+
+    return redirect(get_area_from_pun(pun))
+
+
+def get_area_from_pun(pun):
+    while pun:
+        try:
+            r = requests.get(mapit_url_template.format(pun=pun),
+                             allow_redirects=False)
+            return r.headers.get('location')
+        except requests.exceptions.HTTPError:
+            # strip off last component
+            pun = re.sub(r'[^:]+$', '', pun)
+            pun = re.sub(r':$', '', pun)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- polling_unit_lookup_test.py
+++ polling_unit_lookup_test.py
@@ -1,19 +1,26 @@
 import polling_unit_lookup
 import unittest
+import requests_mock
 
 
-class FlaskrTestCase(unittest.TestCase):
+class PollingUnitLookupTestCase(unittest.TestCase):
 
     def setUp(self):
         polling_unit_lookup.app.config['TESTING'] = True
         self.app = polling_unit_lookup.app.test_client()
 
-    def test_polling_unit_lookup(self):
+    def test_polling_unit_lookup_invalid_number(self):
         rv = self.app.get('/?q=abcd')
-        assert 'ABCD is not a valid polling unit number' in rv.data
-        rv = self.app.get('/?q=AB:01:23:45')
-        print(rv.data)
-        assert 'AB:1:23:45 is a valid polling unit number' in rv.data
+        assert 'abcd is not a valid polling unit number' in rv.data
+
+    def test_polling_unit_lookup_valid_number(self):
+        with requests_mock.mock() as m:
+            m.get('http://www.shineyoureye.org/mapit/code/poll_unit/AB:1:23:45',
+                  headers={
+                      'Location': 'http://www.shineyoureye.org/mapit/area/84'
+                  })
+            rv = self.app.get('/?q=AB:01:23:45')
+            self.assertEqual(rv.headers.get('location'), 'http://www.shineyoureye.org/mapit/area/84')
 
 if __name__ == '__main__':
     unittest.main()
