55d4c9370cbb952b0e5b1fe25e0a683334726384
Chris Mytton <chrismytton@gmail.com>
2016-02-23 17:06:28 +0000

    Return the mapit response rather than redirecting to it
    This gives us control over the response in case mapit changes in the
    future.

--- polling_unit_lookup.py
+++ polling_unit_lookup.py
@@ -1,14 +1,15 @@
+import os
 import re
 
 import requests
-from flask import Flask, request, redirect
+from flask import Flask, request, jsonify
 
 from polling_unit import tidy_up_pun, pun_re
 
 
 app = Flask(__name__)
-
-mapit_url_template = 'http://www.shineyoureye.org/mapit/code/poll_unit/{pun}'
+app.config['MAPIT_API_URL'] = os.environ.get(
+    'MAPIT_API_URL', 'http://www.shineyoureye.org/mapit')
 
 
 @app.route("/")
@@ -17,21 +18,24 @@ def polling_unit_lookup():
     pun = tidy_up_pun(query)
 
     if not pun_re.search(pun):
-        return "{} is not a valid polling unit number".format(query)
+        error = 'Unrecognized poll_unit: {}.'
+        return jsonify(code=404, error=error.format(query)), 404
 
-    return redirect(get_area_from_pun(pun))
+    return jsonify(get_area_from_pun(pun))
+
+
+def mapit_url(pun):
+    return '{mapit}/code/poll_unit/{pun}'.format(mapit=app.config['MAPIT_API_URL'], pun=pun)
 
 
 def get_area_from_pun(pun):
     while pun:
-        try:
-            r = requests.get(mapit_url_template.format(pun=pun),
-                             allow_redirects=False)
-            return r.headers.get('location')
-        except requests.exceptions.HTTPError:
-            # strip off last component
-            pun = re.sub(r'[^:]+$', '', pun)
-            pun = re.sub(r':$', '', pun)
+        r = requests.get(mapit_url(pun))
+        if r.status_code == 200:
+            return r.json()
+        # strip off last component
+        pun = re.sub(r'[^:]+$', '', pun)
+        pun = re.sub(r':$', '', pun)
 
 
 if __name__ == "__main__":
--- polling_unit_lookup_test.py
+++ polling_unit_lookup_test.py
@@ -7,20 +7,21 @@ class PollingUnitLookupTestCase(unittest.TestCase):
 
     def setUp(self):
         polling_unit_lookup.app.config['TESTING'] = True
+        polling_unit_lookup.app.config['MAPIT_API_URL'] = 'http://pmo'
+        polling_unit_lookup.app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
         self.app = polling_unit_lookup.app.test_client()
 
     def test_polling_unit_lookup_invalid_number(self):
         rv = self.app.get('/?q=abcd')
-        assert 'abcd is not a valid polling unit number' in rv.data
+        assert 'Unrecognized poll_unit: abcd' in rv.data
+        self.assertEqual(rv.status_code, 404)
 
     def test_polling_unit_lookup_valid_number(self):
         with requests_mock.mock() as m:
-            m.get('http://www.shineyoureye.org/mapit/code/poll_unit/AB:1:23:45',
-                  headers={
-                      'Location': 'http://www.shineyoureye.org/mapit/area/84'
-                  })
+            m.get('http://pmo/code/poll_unit/AB:1:23:45', text='{"name": "Area"}')
             rv = self.app.get('/?q=AB:01:23:45')
-            self.assertEqual(rv.headers.get('location'), 'http://www.shineyoureye.org/mapit/area/84')
+            self.assertEqual(rv.status_code, 200)
+            self.assertEqual(rv.data, '{"name": "Area"}')
 
 if __name__ == '__main__':
     unittest.main()
