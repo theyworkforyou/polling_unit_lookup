bc6caee57812262e72c03c89cfd02c6013c3a325
Chris Mytton <chrismytton@gmail.com>
2016-02-23 15:49:03 +0000

    Initial working version

--- polling_unit_lookup.py
+++ polling_unit_lookup.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from polling_unit import tidy_up_pun, pun_re
+
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def polling_unit_lookup():
+    pun = tidy_up_pun(request.args.get('q'))
+    if pun_re.search(pun):
+        return "{} is a valid polling unit number".format(pun)
+    else:
+        return "{} is not a valid polling unit number".format(pun)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
